a2855bf13062141cc905b789c7194cb6f87991e9
Avinash Chaturvedi <avinashchaturvedi2002@gmail.com>
2025-04-03 04:06:02 +0530

    removed generatenewword function from the time mode

--- frontend/src/components/TypingTest.jsx
+++ frontend/src/components/TypingTest.jsx
@@ -115,7 +115,6 @@ const TypingTest = ({ user }) => {
                 className={`mx-2 px-4 py-2 rounded-md font-semibold text-lg ${selectedTime === time ? "bg-blue-500 text-white" : "bg-gray-700 text-gray-300"}`}
                 onClick={() => {
                   setSelectedTime(time);
-                  generateNewWords();
                   setTimeout(() => inputRef.current.focus(), 100);
                 }}
               >
